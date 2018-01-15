14b184c3ce96a74fac3a5afb7467f12ac4304456
Nicholas Guerra <nick@scriptthread.com>
2018-01-14 21:50:07 -0800

    first

---
+++ client/web/frontpage.js
@@ -0,0 +1,13 @@
+Template.FrontPage.onCreated(function(){
+	var self = this;
+	self.autorun(function (){
+		self.subscribe('recipes');
+	});
+});
+
+Template.FrontPage.events({
+	'click .dropdown-toggle': function (e) {
+		e.preventDefault();
+		$(e.target).find('.dropdown-menu').toggle();
+	}
+});
